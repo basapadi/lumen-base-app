3c8c3576f47f632116dd3aa50c32db8e2ea18157
bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
2023-04-04 19:10:37 +0700

    Fixing Wording Result

--- app/Traits/TerbilangTrait.php
+++ app/Traits/TerbilangTrait.php
@@ -2,8 +2,8 @@
 namespace App\Traits;
 
 trait TerbilangTrait {
-    private $_bilangan = [" ", "Satu", "Dua", "Tiga", "Empat", "Lima",
-    "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+    private $_bilangan = [" ", " Satu", " Dua", " Tiga", " Empat", " Lima",
+    " Enam", " Tujuh", " Delapan", " Sembilan", " Sepuluh", " Sebelas"];
 
 
 	/**
@@ -13,39 +13,39 @@ trait TerbilangTrait {
      * @return string
      */
 	public function terbilang($val){
-		return $this->convert($val);
+		return trim($this->convert($val));
 	}
 
     private function convert($value){
         if($value < 12){
-			return " ". $this->_bilangan[(int)$value];
+			return "". $this->_bilangan[(int)$value];
 		}
 		else if($value < 20){
-			return $this->convert($value - 10) . " Belas ";
+			return $this->convert($value - 10) . " Belas";
 		}
 		else if($value < 100){
-			return ($this->convert($value / 10) . " Puluh ") .  $this->convert($value % 10); 
+			return ($this->convert($value / 10) . " Puluh") .  $this->convert($value % 10); 
 		}
 		else if($value < 200 ){ 
 			return "Seratus" . $this->convert($value - 100);
 		}
 		else if($value < 1000){
-			return ($this->convert($value / 100) . " Ratus " ) . $this->convert($value % 100);
+			return ($this->convert($value / 100) . " Ratus" ) . $this->convert($value % 100);
 		}
 		else if($value < 2000){
 			return "Seribu" . $this->convert($value - 1000);
 		}
 		else if($value < 1000000){
-			return ($this->convert($value /1000) . " Ribu ") . $this->convert($value % 1000);
+			return ($this->convert($value /1000) . " Ribu") . $this->convert($value % 1000);
 		}
 		else if($value < 1000000000){
-			return ($this->convert($value /1000000) . " Juta ") . $this->convert($value % 1000000);
+			return ($this->convert($value /1000000) . " Juta") . $this->convert($value % 1000000);
 		}
 		else if($value < (double) "1000000000000L"){
-			return ($this->convert($value /1000000000) . " Milyar ") . $this->convert($value % 1000000000);
+			return ($this->convert($value /1000000000) . " Milyar") . $this->convert($value % 1000000000);
 		}
 		else if($value < (double) "1000000000000000L"){
-			return ($this->convert($value / (double) "1000000000000L") . " Triliun ") . $this->convert($value % (double) "1000000000000L");
+			return ($this->convert($value / (double) "1000000000000L") . " Triliun") . $this->convert($value % (double) "1000000000000L");
 		}
 		return null;
     }
