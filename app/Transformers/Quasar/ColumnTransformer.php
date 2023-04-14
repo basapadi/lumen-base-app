e2573eed4f502df7635b58e63e6abb893184a15c
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2023-04-14 23:45:26 +0700

    Update ColumnTransformer.php
    Adding support _limit, _page,_sort on queryFilter

--- app/Transformers/Quasar/ColumnTransformer.php
+++ app/Transformers/Quasar/ColumnTransformer.php
@@ -1,28 +1,189 @@
 <?php
+namespace App\Traits;
 
-namespace App\Transformers\Quasar;
+use Illuminate\Database\Eloquent\Builder as eloBuilder;
 
-use League\Fractal\TransformerAbstract;
-
-class ColumnTransformer extends TransformerAbstract {
+/**
+ * Description of DbTrait
+ * @author bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
+ * @since 
+ */
+trait DbTrait {
+    
+    private $_defaultLimit;
+    
+    public function setDefaultLimit(int $limit) {
+        $this->_defaultLimit = $limit;
+    }
+    
+    /**
+     * limiter record for model
+     * @param Illuminate\Database\Eloquent\Builder $model
+     * @param int $defaultLimit
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function limiter(eloBuilder &$model, int $defaultLimit = 10) {
+        $page = (int) request('page');
+        $limit = (int) request('limit');
+        
+        if ($defaultLimit == -1) {
+            return $model;
+        }
+        
+        if (!empty($this->_defaultLimit)) {
+            $limit = $this->_defaultLimit;
+        } elseif (empty($limit)) {
+            $limit = $defaultLimit;
+        }
+        
+        if ($page > 1) {
+            $skip = ($page - 1) * $limit;
+        } else {
+            $skip = 0;
+        }
+        
+        return $model->skip($skip)->take($limit);
+        
+    }
 
     /**
-    *   parameter format columns:
-    *   $resp = [
-    *       ['value' => 'id', 'label' => 'id','align' => 'left'],
-    *       ...
-    *   ];
-    */
-    public function transform($resp) {
-        $align = 'left';
-        if(isset($resp['align'])) $align = $resp['align'];
-        return [
-            'name' => $resp['value'],
-            'required' => true,
-            'label' => ucwords(implode(' ',explode('_',ucfirst($resp['label'])))),
-            'align' =>  $align,
-            'field' => $resp['value'],
-            'sortable' => true
-        ];
+     * limiter record for model
+     * @param Illuminate\Database\Eloquent\Builder $model
+     * @return Illuminate\Database\Eloquent\Builder
+     */
+    public function queryFilters( &$model){
+        $requests = request()->all(); 
+        $page = 1;
+        $limit = 10;
+        foreach($requests as $key => $value){
+            $relation = explode('__', $key);
+            
+            $table = '';
+            if(count($relation) >= 2) {
+                $key = $relation[1];
+                $table = $relation[0];
+            }
+
+            $_filter = explode("_",$key);
+            if (count($_filter)<=1) continue;
+            $_filter_name = $_filter[count($_filter)-1];
+            unset($_filter[count($_filter)-1]);
+            $column = implode("_",$_filter);
+            
+            switch ($_filter_name) {
+                case 'is':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column, $value);
+                        });
+                    } else $model->where($column,$value);
+                    break;
+                case 'contain':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column,"LIKE","%$value%");
+                        });
+                    } else $model->where($column,"LIKE","%$value%");
+                    break;
+                case 'gte':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column,">=",$value);
+                        });
+                    } else $model->where($column,">=",$value);
+                    break;
+                case 'lte':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column,"<=",$value);
+                        });
+                    } else $model->where($column,"<=",$value);
+                    break;
+                case 'gt':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column,">",$value);
+                        });
+                    } else $model->where($column,">",$value);
+                    break;
+                case 'lt':
+                    $model->where($column,"<",$value);
+                    break;
+                case 'eq':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column,"=",$value);
+                        });
+                    } else $model->where($column,"=",$value);
+                    break;
+                case 'ne':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$value) {
+                            $query->where($column,"<>",$value);
+                        });
+                    } else $model->where($column,"<>",$value);
+                    break;
+                case 'between':
+                    $_between = explode('-',$value);
+                    sort($_between);
+                    $_between = array_map('intval',$_between);
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column,$_between) {
+                            $query->whereBetween($column,$_between);
+                        });
+                    } else $model->whereBetween($column,$_between);
+                    break;
+                case 'null':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column) {
+                            $query->whereNull($column);
+                        });
+                    } else $model->whereNull($column);
+                    break;
+                case 'notnull':
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column) {
+                            $query->whereNotNull($column);
+                        });
+                    } else $model->whereNotNull($column);
+                    break;
+                case 'in':
+                    $_in = explode(',',$value);
+                    $_in = array_map('intval',$_in);
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column, $_in) {
+                            $query->whereIn($column,$_in);
+                        });
+                    } else $model->whereIn($column,$_in);
+                    break;
+                case 'notin':
+                    $_notin = explode(',',$value);
+                    $_notin = array_map('intval',$_notin);
+                    if(!empty($table)) {
+                        $model->whereHas($table, function($query) use ($column, $_notin) {
+                            $query->whereNotIn($column,$_notin);
+                        });
+                    } else $model->whereNotIn($column,$_notin);
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+            
+            if($key == '_page') $page = $value;
+            if($key == '_limit') $limit = $value;
+
+            if ($page > 1)  $skip = ($page - 1) * $limit;
+            else  $skip = 0;
+
+            if($key == '_sort'){
+                $sort = explode(':',$value);
+                if(count($sort) < 2) continue;
+                $model->orderBy(trim($sort[0]),trim($sort[1]));
+            }
+        }
+
+        $model->skip($skip)->take($limit);
+       return $model;
     }
 }
